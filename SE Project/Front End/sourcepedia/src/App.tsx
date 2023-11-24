e10de9d5cc008a98757445e89353c7ccb3cf391e
Cytzr <kelvinethanaelyahja@gmail.com>
2023-11-24 20:12:59 +0700

    front end update
    navbar update

--- SE Project/Front End/sourcepedia/src/App.tsx
+++ SE Project/Front End/sourcepedia/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element=''></Route>
+          <Route path='/write' element=''></Route>
+          <Route path='/login' element=''></Route>
+          <Route path='/register' element=''></Route>
+          <Route path='/about' element=''></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
