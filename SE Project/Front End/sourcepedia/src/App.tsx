3ca2b22f04c4273dd99c041d618e221b5e4703c5
Cytzr <kelvinethanaelyahja@gmail.com>
2023-11-25 20:40:17 +0700

    some optimization
    home is now joined into the pages folder

--- SE Project/Front End/sourcepedia/src/App.tsx
+++ SE Project/Front End/sourcepedia/src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { Navbar } from './Components/Navbar';
-// <<<<<<< HEAD
 import { Login } from './Pages/Login';
 import { Register } from './Pages/Register';
-// =======
 import About from './About/About';
-import Main from './Main/Main';
-// >>>>>>> 271f53b8a77cb412d2173077590e697370f28335
+import Main from './Pages/Main';
 
 function App() {
   return (
@@ -17,15 +14,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Main />}></Route>
           <Route path='/write' element=''></Route>
-{/* <<<<<<< HEAD */}
           <Route path='/login' element={<Login/>}></Route>
           <Route path='/register' element={<Register/>}></Route>
           <Route path='/about' element=''></Route>
-{/* =======
-          <Route path='/login' element=''></Route>
-          <Route path='/register' element=''></Route>
-          <Route path='/about' element={<About />}></Route> */}
-{/* >>>>>>> 271f53b8a77cb412d2173077590e697370f28335 */}
         </Routes>
       </Router>
     </div>
