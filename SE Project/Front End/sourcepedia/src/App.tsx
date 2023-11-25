271f53b8a77cb412d2173077590e697370f28335
narutodatebayo1 <kwntimothy@gmail.com>
2023-11-25 14:12:10 +0700

    add main & about page

--- SE Project/Front End/sourcepedia/src/App.tsx
+++ SE Project/Front End/sourcepedia/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { Navbar } from './Components/Navbar';
+import About from './About/About';
+import Main from './Main/Main';
 
 function App() {
   return (
@@ -8,11 +10,11 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path='/' element=''></Route>
+          <Route path='/' element={<Main />}></Route>
           <Route path='/write' element=''></Route>
           <Route path='/login' element=''></Route>
           <Route path='/register' element=''></Route>
-          <Route path='/about' element=''></Route>
+          <Route path='/about' element={<About />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ SE Project/Front End/sourcepedia/src/Main/Main.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react"
+import "./Main.css"
+
+export default function Main(){
+
+    // dummy data
+    const [tagList, SetTagList] = useState([
+        {
+            tagID: "2",
+            tagName: "Programming"
+        },
+        {
+            tagID: "3",
+            tagName: "Data Science"
+        },
+        {
+            tagID: "2",
+            tagName: "Technology"
+        },
+        {
+            tagID: "3",
+            tagName: "Self Improvement"
+        },
+        {
+            tagID: "2",
+            tagName: "Writing"
+        },
+        {
+            tagID: "3",
+            tagName: "Relationship"
+        },
+        {
+            tagID: "2",
+            tagName: "Machine Learning"
+        },
+        {
+            tagID: "2",
+            tagName: "Productivity"
+        },
+        {
+            tagID: "2",
+            tagName: "Politics"
+        },
+    ])
+    const [postList, SetPostList] = useState([
+        {
+            documentID: "1",
+            title: "ABC",
+            content: "wertyuioplkjhgfdsxcvbn",
+            publisedTime: "2023-11-12"
+        },
+        {
+            documentID: "1",
+            title: "ABC",
+            content: "wertyuioplkjhgfdsxcvbn",
+            publisedTime: "2023-11-12"
+        },
+        {
+            documentID: "1",
+            title: "ABC",
+            content: "wertyuioplkjhgfdsxcvbn",
+            publisedTime: "2023-11-12"
+        },
+        {
+            documentID: "1",
+            title: "ABC",
+            content: "wertyuioplkjhgfdsxcvbn",
+            publisedTime: "2023-11-12"
+        },
+    ])
+
+
+    return (
+        <div className="main">
+
+            <div className="main-header">
+                <div className="left-header">
+                    <h1>Elevate your understanding.</h1>
+                </div>
+                <div className="right-header">
+                    <p>Discover more of what matters to you</p>
+                    <div className="tag-list-div">
+                        {tagList.map((tag, id) => {
+                            return (
+                                <div key={id}>
+                                    {tag.tagName}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+
+            <div className="post-list-div">
+                {postList.map((post) => {
+                    return (
+                        <div>
+                            <p>{post.title}</p>
+                            <p>{post.publisedTime}</p>
+                        </div>
+                    )
+                })}
+            </div>
+
+        </div>
+    )
+}
