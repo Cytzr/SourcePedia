8afc20547d31d33b65fa244ce2767ef774311709
narutodatebayo1 <109846615+narutodatebayo1@users.noreply.github.com>
2023-11-25 17:50:58 +0700

    add write page

--- SE Project/Front End/sourcepedia/src/App.tsx
+++ SE Project/Front End/sourcepedia/src/App.tsx
@@ -5,8 +5,9 @@ import { Navbar } from './Components/Navbar';
 import { Login } from './Pages/Login';
 import { Register } from './Pages/Register';
 // =======
-import About from './About/About';
-import Main from './Main/Main';
+import About from './Pages/About/About';
+import Main from './Pages/Main/Main';
+import Write from './Pages/Write/Write';
 // >>>>>>> 271f53b8a77cb412d2173077590e697370f28335
 
 function App() {
@@ -16,11 +17,11 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Main />}></Route>
-          <Route path='/write' element=''></Route>
+          <Route path='/write' element={<Write />}></Route>
 {/* <<<<<<< HEAD */}
           <Route path='/login' element={<Login/>}></Route>
           <Route path='/register' element={<Register/>}></Route>
-          <Route path='/about' element=''></Route>
+          <Route path='/about' element={<About />}></Route>
 {/* =======
           <Route path='/login' element=''></Route>
           <Route path='/register' element=''></Route>
