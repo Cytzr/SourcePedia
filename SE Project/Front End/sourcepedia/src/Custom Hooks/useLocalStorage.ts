076298c7b764f077030e5c5d58fca8a4fc5571f7
Cytzr <kelvinethanaelyahja@gmail.com>
2023-11-28 15:44:24 +0700

    useLocalStorage Hook

--- SE Project/Front End/sourcepedia/src/Custom Hooks/useLocalStorage.ts
+++ SE Project/Front End/sourcepedia/src/Custom Hooks/useLocalStorage.ts
@@ -1,5 +1,29 @@
-export const addLocal = () =>{
-    return (
-        1
-    )
+export const useLocalStorage = (key:string) =>{
+    const setItem = (value:unknown)=>{
+        try {
+            window.localStorage.setItem(key, JSON.stringify(value)); 
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getItem = () =>{
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : undefined;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const removeItem = () =>{
+        try {
+            window.localStorage.removeItem(key);
+            window.localStorage.setItem(key, JSON.stringify([""])); 
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return { setItem, getItem, removeItem}
 }
